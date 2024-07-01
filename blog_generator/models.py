a16c906733f4750d9855e2b439a467bcc1d16f62
Rafa Gasull <rafael.gasull@gmail.com>
2024-07-01 21:36:26 +0200

    feat: implement generate-blog endpoint

--- blog_generator/models.py
+++ blog_generator/models.py
@@ -1,4 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
-rafa_example = ['rafa', 'afa']
+class BlogPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    youtube_title = models.CharField(max_length=300)
+    youtube_link = models.URLField()
+    generated_contend = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.youtube_title
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -6,6 +7,12 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
+from openai import OpenAI
+from pytube import YouTube
+
+from ai_blog_app import settings
+
+from .models import BlogPost
 
 
 # Create your views here.
@@ -20,21 +27,73 @@ def generate_blog(request):
         try:
             data = json.loads(request.body)
             youtube_link = data['link']
-            return JsonResponse({'content': youtube_link}, status=200)
         except (KeyError, json.JSONDecodeError):
             return JsonResponse({'error': 'Invalid data sent'}, status=400)
 
         # get youtube title
+        title = _youtube_title(link=youtube_link)
 
         # get transcript
+        transcription = _get_transcription(link=youtube_link)
+        if not transcription:
+            return JsonResponse({'error': 'Failed to get transcription'}, status=500)
 
         # use OpenAI to generate the blog
+        blog_content = _generate_blog_from_transcription(transcription)
+        if not blog_content:
+            return JsonResponse({'error': 'Failed to generate blog article'}, status=500)
 
         # save blog article to database
+        new_blog_article = BlogPost.objects.create(
+            user=request.user,
+            youtube_title=title,
+            youtube_link=youtube_link,
+            generated_contend=blog_content,
+        )
+        new_blog_article.save()
 
         # return blog article as a response
+        return JsonResponse({'content': blog_content}, status=200)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+
+def _youtube_title(link: str) -> str:
+    youtube = YouTube(url=link)
+    return youtube.title
+
+
+def _download_audio(link: str):
+    youtube = YouTube(url=link)
+    if video := youtube.streams.filter(only_audio=True).first():
+        out_file = video.download(output_path=settings.MEDIA_ROOT)
+        base, _ = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        return new_file
+
+
+def _get_transcription(link: str):
+    audio_file_path = _download_audio(link)
+    client = OpenAI()
+    with open(audio_file_path, 'rb') as audio_file:
+        transcription = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+    return transcription.text
+
+
+def _generate_blog_from_transcription(transcription: str):
+    client = OpenAI()
+    completion = client.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following transcription that belongs to a youtube Video and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points.",
+            },
+            {"role": "user", "content": transcription},
+        ],
+    )
+    return completion.choices[0].message.content
 
 
 def user_login(request):
