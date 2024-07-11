871b96e3cc7280c5bff0678133f118189767797f
Rafa Gasull <rafael.gasull@gmail.com>
2024-07-11 14:31:18 +0200

    Fix pytube bug regex

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'
 
-ALLOWED_HOSTS = ['localhost', 'aiblogapp-production.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'aiblogapp-production.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://aiblogapp-production.up.railway.app']
 
